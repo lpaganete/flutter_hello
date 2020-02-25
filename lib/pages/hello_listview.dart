d8029f2131d2b981ea9804c632fe0876cbed3a4d
Luis Paulo Aganete <luispaulo.aganete@hotmail.com>
2020-02-24 21:19:09 -0300

    Implementando SatatefulWidget

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -7,24 +7,36 @@ class Dog {
 }
 
 
-class HelloListView extends StatelessWidget {
+class HelloListView extends StatefulWidget {
+
+  @override
+  _HelloListViewState createState() => _HelloListViewState();
+}
+
+class _HelloListViewState extends State<HelloListView> {
+
+
+      bool _gridView = true; 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text("ListView"),
+        centerTitle: true,
         actions: <Widget>[ //Arrey de widgets que permite colocar qualquer widget na appbar
           IconButton(icon: Icon(Icons.list), onPressed: (){  //Class IconButton adiciona botoes a appbar
             print("Lista");
+            _gridView = false; // Se eu clicar no botão da lista, ele vai retornar para variavel false
+
           },),
            IconButton(icon: Icon(Icons.grid_on), onPressed: (){
             print("Grid");
+            _gridView = true; // se eu clicar no botão da grid, ele vai retornar true
           },)
         ],
-        title: Text(
-            "ListView"
-        ),
-        centerTitle: true,
       ),
+      
       body: _body(),
     );
   }
@@ -38,39 +50,56 @@ class HelloListView extends StatelessWidget {
       Dog("Pastor", "assets/images/dog5.png"),
     ];
 
-    return GridView .builder( 
+    if(_gridView){
+      return GridView.builder( 
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),  //Adciona mais colunas a listView
         itemCount: dogs.length,
         itemBuilder: (context, index) { // Responsavel por retornar o layout (widget) dessa celula
          
-          Dog dog = dogs[index]; //recuperando o objeto cachorro
-
-          return Stack( // Funciona como uma pilha
-            fit: StackFit.expand, //Expande a imagem até o máximo (no caso 300px que foi definido)
-            children: <Widget>[
-              _img(dog.foto), // A imagem sempre tem que vir antes do nome
-              Align( // Para mudar a posição do texto 
-                alignment: Alignment.topLeft,
-                      child: Container(
-                      margin: EdgeInsets.all(12), // define uma margem para o container
-                      padding: EdgeInsets.all(8),
-                      decoration: BoxDecoration( // Possibilita a personalização do container 
-                        color: Colors.black45,
-                        borderRadius: BorderRadius.circular(16), //Adiciona uma borda arredondada
-                      ),
-                        child: Text(dog.nome,
-                        style: TextStyle(fontSize: 26, color: Colors.white),
-                      ),
-                ),
-              ),
-            ],
-          );  
+          return _itemView(dogs, index);  
           
        },
     );
+    }else {
+      return ListView.builder( 
+        itemExtent: 300,
+        itemCount: dogs.length,
+        itemBuilder: (context, index) { // Responsavel por retornar o layout (widget) dessa celula
+         
+          return _itemView(dogs, index);  
+          
+       },
+    );
+    }
+
   }
 
-  Widget _img(String img) {
+  _itemView(List<Dog> dogs, int index) {
+    Dog dog = dogs[index]; //recuperando o objeto cachorro
+    
+    return Stack( // Funciona como uma pilha
+      fit: StackFit.expand, //Expande a imagem até o máximo (no caso 300px que foi definido)
+      children: <Widget>[
+        _img(dog.foto), // A imagem sempre tem que vir antes do nome
+        Align( // Para mudar a posição do texto 
+          alignment: Alignment.topLeft,
+                child: Container(
+                margin: EdgeInsets.all(12), // define uma margem para o container
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration( // Possibilita a personalização do container 
+                  color: Colors.black45,
+                  borderRadius: BorderRadius.circular(16), //Adiciona uma borda arredondada
+                ),
+                  child: Text(dog.nome,
+                  style: TextStyle(fontSize: 26, color: Colors.white),
+                ),
+          ),
+        ),
+      ],
+    ); 
+  }
+
+   _img(String img) {
     return Image.asset(
       img,
       fit: BoxFit.cover,
